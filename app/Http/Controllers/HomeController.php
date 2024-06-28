796d3be9b034188d56521dd1c2f4904f89852da4
Naveed Shahzad <lhrciit@gmail.com>
2024-06-28 16:00:27 +0500

    build

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,7 +34,7 @@ class HomeController extends Controller
         $series = array();
 
         $raws = ['Rahim Yar Khan Industrial Estate', 'Bhalwal Industrial Estate', 'Vehari Industrial Estate', 'Quaid-e-Azam Business Park',
-            'M-3 Industrial City', 'Vehari Industrial City', 'Allama Iqbal Industrial City'];
+            'M-3 Industrial City', 'Value Addition City', 'Allama Iqbal Industrial City'];
         $users = array();
         foreach ($raws as $key=>$value){
             $user = new \stdClass;
@@ -69,7 +69,7 @@ class HomeController extends Controller
         $series = array();
 
         $raws = ['Rahim Yar Khan Industrial Estate', 'Bhalwal Industrial Estate', 'Vehari Industrial Estate', 'Quaid-e-Azam Business Park',
-            'M-3 Industrial City', 'Vehari Industrial City', 'Allama Iqbal Industrial City'];
+            'M-3 Industrial City', 'Value Addition City', 'Allama Iqbal Industrial City'];
         $users = array();
         foreach ($raws as $key=>$value){
             $user = new \stdClass;
